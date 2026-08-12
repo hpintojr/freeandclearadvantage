d4e21c5eb5135aa0aa9efa17289e91a2ce4d1ac2
hpintojr <hpintojr@gmail.com>
2026-08-12 14:50:49 -0700

    Use 30-minute booking starts from 9 AM

--- lib/booking.ts
+++ lib/booking.ts
@@ -1,5 +1,23 @@
-export const defaultSlotStarts = ["09:30", "10:30", "11:30", "12:30", "13:30", "14:30", "16:00"];
+export const defaultSlotStarts = [
+  "09:00",
+  "09:30",
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
+  "14:30",
+  "15:00",
+  "15:30",
+  "16:00",
+  "16:30",
+];
 export const slotDurationMinutes = 60;
+export const slotIntervalMinutes = 30;
 
 export function addMinutesIso(iso: string, minutes: number) {
   return new Date(new Date(iso).getTime() + minutes * 60_000).toISOString();
